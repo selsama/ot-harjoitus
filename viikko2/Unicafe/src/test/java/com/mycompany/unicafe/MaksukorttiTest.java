53adb024da03f2602ffc9ffc99bac3dce3d90c4c
S Salmi <56793802+selsama@users.noreply.github.com>
2020-03-20 16:08:51 +0200

    tehtävä 3 jacoco

--- viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,40 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(100);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void alkusaldoOikein(){
+        assertEquals(100, kortti.saldo());
+    }
+    
+    @Test
+    public void toStringIlmoittaaSaldonOikein(){
+        assertEquals("saldo: 1.0", kortti.toString());
+    }
+    
+    @Test
+    public void lataaminenKasvattaaSaldoaOikein(){
+        kortti.lataaRahaa(200);
+        assertEquals(300,kortti.saldo());
+    }
+    
+    @Test
+    public void otaRahaaVähentääSaldoaOikein(){
+        kortti.otaRahaa(50);
+        assertEquals(50, kortti.saldo());
+    }
+    
+    @Test
+    public void otaRahaaKertooRiittivatkoRahat(){
+        assertEquals(true, kortti.otaRahaa(50));
+        assertEquals(false, kortti.otaRahaa(200));
+        assertEquals(true, kortti.otaRahaa(50));
     }
 }
