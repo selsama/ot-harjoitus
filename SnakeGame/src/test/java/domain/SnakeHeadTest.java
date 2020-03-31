b4a3b3226958e289b0ed3101291271f57b314b36
S Salmi <56793802+selsama@users.noreply.github.com>
2020-03-31 16:37:06 +0300

    snakehead tests

--- SnakeGame/src/test/java/domain/SnakeHeadTest.java
+++ SnakeGame/src/test/java/domain/SnakeHeadTest.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import snakegame.domain.SnakeHead;
 
 /**
  *
@@ -18,28 +19,74 @@ import static org.junit.Assert.*;
  */
 public class SnakeHeadTest {
     
-    public SnakeHeadTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+    SnakeHead head;
+
     @Before
     public void setUp() {
+        head = new SnakeHead(10,10);
     }
     
-    @After
-    public void tearDown() {
-    }
-
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
-//    //
+    //
+    
+    @Test
+    public void snakeHeadTurnLeftWorks(){
+        head.turnLeft();
+        double end = head.getHead().getRotate();
+        assertEquals(-90,end,0.01);
+    }
+    
+    @Test
+    public void snakeHeadTurnLeftWontTurnIfHeadedRight(){
+        head.getHead().setRotate(90);
+        head.turnLeft();
+        assertEquals(90,head.getHead().getRotate(),0.01);
+    }
+    
+    @Test
+    public void snakeHeadTurnRightWorks(){
+        head.turnRight();
+        double end = head.getHead().getRotate();
+        assertEquals(90,end,0.01);
+    }
+    
+    @Test
+    public void snakeHeadTurnRightWontTurnIfHeadedLeft(){
+        head.getHead().setRotate(-90);
+        head.turnRight();
+        assertEquals(-90,head.getHead().getRotate(),0.01);
+    }
+    
+    @Test
+    public void snakeHeadTurnDownWorks(){
+        head.getHead().setRotate(90);
+        head.turnDown();
+        double end = head.getHead().getRotate();
+        assertEquals(180,end,0.01);
+    }
+    
+    @Test
+    public void snakeHeadTurnDownWontTurnIfHeadedUp(){
+        head.getHead().setRotate(0);
+        head.turnDown();
+        assertEquals(-0,head.getHead().getRotate(),0.01);
+    }
+
+    @Test
+    public void snakeHeadTurnUpWorks(){
+        head.getHead().setRotate(90);
+        head.turnUp();
+        assertEquals(0,head.getHead().getRotate(),0.01);
+    }
+    
+    @Test
+    public void snakeHeadTurnUpWontTurnIfHeadedDown(){
+        head.getHead().setRotate(180);
+        head.turnUp();
+        assertEquals(180,head.getHead().getRotate(),0.01);
+    }
+    
 //     @Test
 //     public void snakeHeadBeginsInMiddle() {
 //     }
@@ -48,7 +95,7 @@ public class SnakeHeadTest {
 //     public void snakeHeadDoesNotMoveRightAway(){
 //         
 //     }
-//     
+//     These are higher level tests, i think
 //     @Test
 //     public void snakeHeadMovesWhenKeyIsPressed(){
 //         
